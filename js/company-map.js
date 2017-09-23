2f23281d960ae6ec747f626ab919038d2d6d4771
Nikos Perivolaris <nikosper.gm@gmail.com>
2017-09-23 12:36:45 +0200

    bugs fixed

--- js/company-map.js
+++ js/company-map.js
@@ -1,21 +1,23 @@
 function myMap() {
     var mapCanvas = document.getElementById("map");    
-    var geocoder;
-    geocoder = new google.maps.Geocoder();
+     var geocoder;
+     geocoder = new google.maps.Geocoder();
+     var latlng = new google.maps.LatLng(38.401291, 24.039005);
 
-    geocoder.geocode( { 'address': 'Περιφερειακή Οδός Αλιβερίου, Αλιβέρι, Ελλάδα'}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
+     geocoder.geocode( { 'address': 'Περιφερειακή Οδός Αλιβερίου, Αλιβέρι, Ελλάδα'}, function(results, status) {
+       if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(latlng);
         var marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: latlng
         });
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
-      }
+     }
     });
+
     var mapOptions = ({
-        //center: myCenter,
+        center: new google.maps.LatLng(24.039005, 38.401291),
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.HYBRID
     });
